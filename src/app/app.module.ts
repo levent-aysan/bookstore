ce02528ac67e11d4295973597774da5390e463f4
DESKTOP-QAI8JAG\Q <turkmuhendis1@gmail.com>
2017-12-02 21:46:26 +0000

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
